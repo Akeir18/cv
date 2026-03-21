fbf502c0e1410b413d38df949efc45ef87f2d3f5
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2026-03-21 22:57:16 +0100

    feat(header-component): Creando el componente para el header

---
+++ src/assets/components/layout/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>Header</h1>
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    background-color: $bg-secondary;
+    color: $text-main;
+    height: 75px;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    padding: 0px $spacing-xs;
+
+    @include responsive(mobile) {
+        padding: 0px $spacing-sm;
+    }
+
+    @include responsive(tablet) {
+        padding: 0px $spacing-md;
+    }
+
+    @include responsive(desktop) {
+        padding: 0px $spacing-lg;
+    }
+
+    @include responsive(wide) {
+        padding: 0px $spacing-xl;
+    }
+}
+</style>
