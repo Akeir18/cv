606eb6b3fb28a563031b2d92fc9f3755f4043266
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2026-03-21 22:57:34 +0100

    feat(footer-component): Creando el componente para el footer

---
+++ src/assets/components/layout/FooterComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer>
+        <h1>Footer</h1>
+    </footer>
+</template>
+
+<style lang="scss">
+footer {
+    background-color: $bg-secondary;
+    color: $text-main;
+    height: 75px;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    padding: 0px $spacing-xs;
+
+    @include responsive(mobile) {
+        padding: 0px $spacing-sm;
+    }
+
+    @include responsive(tablet) {
+        padding: 0px $spacing-md;
+    }
+
+    @include responsive(desktop) {
+        padding: 0px $spacing-lg;
+    }
+
+    @include responsive(wide) {
+        padding: 0px $spacing-xl;
+    }
+}
+</style>
