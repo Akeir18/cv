63724c0fafda26ec9b88050745e472d87b3c71f4
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2026-05-14 13:24:30 +0200

    feat(skills-component): Componente para alistar las habilidades
    Recibiendo un título y un array de habilidades, las lista todas utilizando el componente de la puntuación

---
+++ src/assets/components/layout/ui/SkillsList.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import type { Skill } from '@/types/skills';
+import PunctuationComponent from '../base/PunctuationComponent.vue';
+
+
+const props = defineProps<{
+    title: string,
+    skills: Skill[]
+}>()
+
+</script>
+
+<template>
+    <h2>{{ title }}</h2>
+    <ul>
+        <li v-for="skill in skills" :key="skill.name">
+            <strong>{{ skill.name }}</strong> <punctuation-component :value="skill.level" />
+        </li>
+    </ul>
+</template>
